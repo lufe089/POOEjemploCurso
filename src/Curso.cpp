be3bea048db52b796d1738420525ee80d465c31f
Luisa Rincon <lufe089@gmail.com>
2025-10-03 08:33:11 -0500

    Fix constructor to correctly assign nombreCurso
    Updated constructor to use 'this' pointer for 'nombreCurso'.

--- src/Curso.cpp
+++ src/Curso.cpp
@@ -1,21 +1,15 @@
-//
-// Created by zalon on 5/03/2025.
-//
-
 #include "Curso.h"
 
 #include <iostream>
-#include <ostream>
 
-Curso::Curso(std::string nombreCurso, int codigo){
-    Curso::nombreCurso = nombreCurso;
+Curso::Curso(std::string nombreCurso, int codigo){    
+    // Variables de instancia
+    this ->nombreCurso = nombreCurso;
     this->codigo = codigo;
 }
 
 void Curso::asignarSalon(Salon salon) {
     this->salon = salon;
-    // Dos formas de hacer lo mismo Curso::salon = salon;
-
 }
 
 void Curso::mostrarInfo() {
